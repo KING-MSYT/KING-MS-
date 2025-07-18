d1c8eb3e44fa1b70b8bb97da8a2c8e5b43a78dd6
KING-MSYT <indonesiaffacc15@gmail.com>
2025-07-18 19:04:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-// prifix ==========================
-const prefix = '.' 
-// owner nomber ==================
-const ownerNumber = ['94767861594']
-
-SESSION_ID:                process.env.SESSION_ID                 " ",
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN  " ",   // true or fals
-ALIVE_IMG:                 process.env.ALIVE_IMG                 " ",
-ALIVE_MSG:                process.env.ALIVE_MSG                 " "
-
+SESSION_ID: process.env.SESSION_ID || "Pw4y2IgY#bdSSQtp0S5YoWg6ToYOsOGipm84owVPBJVLjruex19o",
 };
