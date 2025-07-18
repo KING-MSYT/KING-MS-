347089fd0a49ad492f1290339e815d443cd75814
KING-MSYT <indonesiaffacc15@gmail.com>
2025-07-18 18:56:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+// prifix ==========================
+const prefix = '.' 
+// owner nomber ==================
+const ownerNumber = ['94767861594']
+
+SESSION_ID:                process.env.SESSION_ID                 " ",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN  " ",   // true or fals
+ALIVE_IMG:                 process.env.ALIVE_IMG                 " ",
+ALIVE_MSG:                process.env.ALIVE_MSG                 " "
+
+};
