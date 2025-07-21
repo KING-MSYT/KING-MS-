f74cb5a172356ebf82ee28ab1cff23ef31ee1cf7
KING-MSYT <indonesiaffacc15@gmail.com>
2025-07-21 21:42:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "iANwRZCT#yZwy4t0xkCjKTr_NOsuz5B59HzuPNEtTN6hpRGh8yNs",
+SESSION_ID: process.env.SESSION_ID || "iAk3kZRT#ttfyA9nLn6HNaWRP3GpLfCRa8frTuwNDBc7A2veShFM",
 };
