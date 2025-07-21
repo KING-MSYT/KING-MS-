692ead3f8a86ad3249575df8aaa2ef87b913d29e
KING-MSYT <indonesiaffacc15@gmail.com>
2025-07-21 20:55:44 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -3,19 +3,16 @@ const { cmd } = require('../command');
 cmd({
   pattern: "ping",
   react: "🏓",
-  desc: "Check bot response speed (edited reply)",
+  desc: "Check bot response speed",
   category: "general",
   filename: __filename
 }, async (conn, mek, m, { reply }) => {
   const start = new Date().getTime();
 
-  const msg = await conn.sendMessage(m.chat, { text: "```Testing Ping ⚡...```" }, { quoted: mek });
+  await conn.sendMessage(m.chat, { text: "```Testing Ping ⚡...```" }, { quoted: mek });
 
   const end = new Date().getTime();
   const speed = end - start;
 
-  await conn.sendMessage(m.chat, {
-    text: `🏓 *Pong!*\n💡 Speed: ${speed}ms\n>ᴋɪɴɢ-ᴍꜱ`,
-    edit: msg.key
-  });
+  await reply(`🏓 *Pong!*\n💡 Speed: ${speed}ms\n>ᴋɪɴɢ-ᴍꜱ`);
 });
