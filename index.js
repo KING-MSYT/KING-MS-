5b739d47a6afe8c3cf563602bef5c6795bb2ffac
KING-MSYT <indonesiaffacc15@gmail.com>
2025-07-27 21:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94767861594']
+const ownerNumber = ['94779415698']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
@@ -55,7 +55,7 @@ const conn = makeWASocket({
 conn.ev.on('connection.update', (update) => {
 const { connection, lastDisconnect } = update
 if (connection === 'close') {
-if (lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
 connectToWA()
 }
 } else if (connection === 'open') {
